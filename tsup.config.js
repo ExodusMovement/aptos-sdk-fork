91ce4cd0b1f986fe20082dd197691efecf4f23d3
David Fraga <david.fraga610@hotmail.com>
2024-01-16 07:31:13 -0600

    fix: exclude sourceMaps from build

--- tsup.config.js
+++ tsup.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   splitting: false,
-  sourcemap: true,
+  sourcemap: false,
   target: "es2018",
 });
